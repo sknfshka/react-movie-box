1b2711329574dc7d42dd1233ef20f5b879870e72
Мария <maria@MacBook-Pro-Konstantin.local>
2018-11-19 18:26:42 +0300

    User profile icon in header

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -6,6 +6,7 @@ const Header = () => {
     <navbar className="nav-sticky">
       <Link to='/'><img className="navbar-login-icon" src="/images/video-camera-white.svg" role="presentation" /></Link>
       <Link to='/' className="navbar-login-title">MovieBox</Link>
+      <Link to='/profile'><img className="navbar-user-icon" src="/images/profile.svg" role="presentation" /></Link>
     </navbar>
   );
 }
