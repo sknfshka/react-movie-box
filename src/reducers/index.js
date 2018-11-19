279b1c5ecec6e34c0e156ab959f356d7fecee322
akhromov <artem.khromov@t-systems.ru>
2018-11-19 22:01:10 +0300

    remove tmp staff

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
-import tracks from './tmp/tracks';
 import filterFilms from './films.filter.reducer';
 import login from './auth.reducer';
 import films from './films.reducer';
@@ -9,7 +8,6 @@ import user from './user.reducer';
 
 export default combineReducers({
   routing: routerReducer,
-  tracks,
   filterFilms,
   films,
   login,
